c4e4d64d10842646c603de8a6066b61fed147eda
Marios Antonoudiou <mariosant@sent.com>
2020-12-22 17:43:30 +0200

    Clean existing account when re-installing

--- src/install.js
+++ src/install.js
@@ -2,12 +2,12 @@ const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 const { path } = require('ramda')
 const day = require('dayjs')
-const {getUri, getToken} = require('./livechat-auth-client')
+const { getUri, getToken } = require('./livechat-auth-client')
 
 const db = admin.firestore()
 const accounts = db.collection('accounts')
 
-const installFunctions = options => {
+const installFunctions = (options) => {
 	const authorize = (_req, res) => {
 		const url = getUri()
 
@@ -20,6 +20,8 @@ const installFunctions = options => {
 		)
 
 		const tokenExpiresAt = day().add(data.expires_in, 'second').toDate()
+		
+		await accounts.doc(String(data.license_id)).delete().catch(() => {})
 
 		await accounts.doc(String(data.license_id)).set({
 			accessToken: data.access_token,
@@ -36,7 +38,10 @@ const installFunctions = options => {
 		res.redirect(options.thankYouUrl)
 	}
 
-	return {authorize: functions.https.onRequest(authorize), callback: functions.https.onRequest(callback)}
+	return {
+		authorize: functions.https.onRequest(authorize),
+		callback: functions.https.onRequest(callback),
+	}
 }
 
 module.exports = installFunctions
