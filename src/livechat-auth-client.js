60ed3fb710c90b57d1b6cd62fc8c299c40350268
Marios Antonoudiou <mariosant@sent.com>
2020-12-28 23:49:22 +0200

    Fix issue with refreshing access token

--- src/livechat-auth-client.js
+++ src/livechat-auth-client.js
@@ -1,8 +1,6 @@
 const ClientOAuth2 = require('client-oauth2')
-const axios = require('axios')
-const FormData = require('form-data')
 const { config } = require('firebase-functions')
-const day = require('dayjs')
+const dayjs = require('dayjs')
 
 const authorizationURL = 'https://accounts.livechatinc.com'
 const tokenURL = 'https://accounts.livechatinc.com/token'
@@ -24,44 +22,33 @@ const livechatAuth = new ClientOAuth2({
 })
 
 const useCachedToken = (fn) => async (accountRef) => {
-	const accountDoc = await accountRef.get()
-	const { accessToken, tokenExpiresAt } = accountDoc.data()
+	const {tokenExpiresAt, token: t} = (await accountRef.get()).data()
+	const token = livechatAuth.createToken(JSON.parse(t))
+	
+	token.expiresIn(tokenExpiresAt.toDate())
 
-	const expectedDateTime = day().subtract(1, 'hour')
-
-	return day(tokenExpiresAt.toDate()).isAfter(expectedDateTime)
-		? accessToken
-		: fn(accountRef)
+	return token.expired()
+		? fn(accountRef)
+		: token.accessToken
 }
 
 const refreshToken = async (accountRef) => {
-	const { refreshToken } = (await accountRef.get()).data()
+	const {token: t} = (await accountRef.get()).data()
 
-	const bodyFormData = new FormData()
+	const oldToken = livechatAuth.createToken(JSON.parse(t))
+	const token = await oldToken.refresh()
 
-	bodyFormData.append('grant_type', 'refresh_token')
-	bodyFormData.append('refresh_token', refreshToken)
-	bodyFormData.append('client_id', clientId)
-	bodyFormData.append('client_secret', clientSecret)
+	await accountRef.set({
+		accessToken: token.accessToken,
+		token: JSON.stringify(token.data),
+		tokenExpiresAt: dayjs().add(token.data.expires_in, 'second').toDate()
+	}, {merge: true})
 
-	const { data } = await axios.post(tokenURL, bodyFormData, {
-		headers: {
-			...bodyFormData.getHeaders(),
-		},
-	})
-
-	const freshAccessToken = data.access_token
-
-	await accountRef.update({
-		accessToken: freshAccessToken,
-		tokenExpiresAt: day().add(28800, 'second').toDate(),
-	})
-
-	return freshAccessToken
+	return token.accessToken
 }
 
 module.exports = {
 	getUri: livechatAuth.code.getUri,
 	getToken: (...args) => livechatAuth.code.getToken(...args),
-	getAccessToken: useCachedToken(refreshToken)
+	getAccessToken: useCachedToken(refreshToken),
 }
